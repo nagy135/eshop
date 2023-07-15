1958f347ba1e9863d04fe98455605261eed6de22
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-07-15 21:20:01 +0200

    feat: items

--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.core.serializers import serialize
+
+from .models import Item
 
 
 def index(request):
-    return HttpResponse(b"Hello, world. You're at the polls index.")
+    return HttpResponse(b"Hello world")
+
+
+def items(request):
+    items = Item.objects.all()
+    data = serialize('json', items)
+    return HttpResponse(data,kcontent_type='application/json')
