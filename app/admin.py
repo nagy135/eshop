c2b15f6e79f94aca0e57e6fc2ee97d1fd4ba0786
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-07-15 21:20:04 +0200

    feat: admin registers

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 
-from .models import Item
+from .models import (
+    User,
+    Bucket,
+    Order,
+    Item,
+    BucketItem,
+    Image
+)
 
+admin.site.register(User)
+admin.site.register(Bucket)
+admin.site.register(Order)
 admin.site.register(Item)
+admin.site.register(BucketItem)
+admin.site.register(Image)
