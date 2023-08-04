01eccb9942c0d3b19a57c6f63b0429a6b9dda78e
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-08-05 01:50:37 +0200

    feat: util collect_related

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 from django.db.models import Model
 from django.core.serializers import serialize
 
@@ -8,3 +9,20 @@ def serialize_model(model: Model) -> str:
     struct = json.loads(data)
     data = json.dumps(struct[0])
     return data
+
+
+def collect_related(li: list, key: str, target: str) -> list:
+    res = {}
+
+    for item in li:
+        key_i = item[key]
+        if key_i in res:
+            print(f"extending {key_i}")
+            res[key_i][target].append(item[target])
+        else:
+            print(f"adding {key_i}")
+            res[key_i] = item
+            res[key_i][target] = [item[target]] if item[target] is not None else []
+        pprint(res)
+
+    return [res[k] for k in res]
